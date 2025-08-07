ed08f9153ecfebbbbd1fd5e541eb5a5af27be0d4
Samrudhi03-hub <samrujagadale@gmail.com>
2025-08-07 17:06:28 +0530

    Developed join us section of the website

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from 'react';
+import './Join.css';
+import emailjs from '@emailjs/browser'
+
+const Join = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+        .sendForm('service_b8gtg6p', 'template_yvyk3u8', form.current, {
+            publicKey: '_9Gxyew21M8Clv8WK',
+        })
+        .then(
+            () => {
+            console.log('SUCCESS!');
+            },
+            (error) => {
+            console.log('FAILED...', error.text);
+            },
+        );
+    };
+  return (
+    <div className="Join" id="join-us">
+        <div className="left-j">
+            <hr />
+            <div>
+                <span className='stroke-text'>READY TO</span>
+                <span>LEVEL UP</span>
+            </div>
+            <div>
+                <span className='stroke-text'>YOUR BODY</span>
+                <span>WITH US?</span>
+            </div>
+        </div>
+        <div className="right-j">
+
+            <form ref={form} className="email-container" onSubmit={sendEmail}>
+                <input type="email" name='user_email' placeholder='Enter Your Email Address' />
+                <button className='btn btn-j'>Join Now</button>
+            </form>
+
+        </div>
+    </div>
+  )
+}
+
+export default Join
